766cdfe5649b6155998320e99599a2953a8330b0
Rodrigo Franco <rodri.franco05@gmail.com>
2026-02-05 12:45:30 -0300

    img

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://rodrigofranco05.github.io/ToursConAmigos-web/',
+  base: '/ToursConAmigos-web/',
   server: {
     port: 3000,
     open: true
